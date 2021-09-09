4e4b0e72dc437cf6c74bc079df291d4f9dac2e8c
manavdesai27 <manav27202@gmail.com>
2021-09-09 12:32:59 +0530

    Feat: Delete, Add, Show

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Note = require("./models/note");
+const notesRouter = require("./routes/notes");
+const methodOverride = require('method-override');
 require("dotenv").config();
 
 const app = express();
 
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
-app.set('view engine', 'ejs');
+app.use(methodOverride('_method'));
+
+app.get("/", async (req, res) => {
+  res.redirect("/all");
+});
+
+app.get("/all", async (req, res) => {
+  const notes = await Note.find().sort("-createdAt");
+  res.render("index", { notes: notes });
+});
 
 mongoose.connect(process.env.SERVER, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+app.use("/", notesRouter);
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started...");
-});
-
-app.get("/all", (req, res) => {
-  res.send("Route - All Working...");
 });
